e45fef8f563ed68116738f53dc24533b22507a27
Noel Quintos <npquintos@gmail.com>
2022-10-04 22:20:56 -0600

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -64,28 +64,28 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } }, /* focus to next window */
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } }, /* focus to previous window */
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, /*  resize left */
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, /*  resize right */
+	{ MODKEY,                       XK_Return, zoom,           {0} }, /* not useful */
+	{ MODKEY,                       XK_Tab,    view,           {0} }, /* not useful */
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} }, /* close window */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* minimize or expose */
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /* maximize window */
+	{ MODKEY,                       XK_space,  setlayout,      {0} }, /* not useful */
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, /* not useful */
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } }, /* show all windows on all tags */
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, /* not useful */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, /* go to other monitor */
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } }, /* go to other monitor */
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, /* not useful */
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, /* not useful */
+	TAGKEYS(                        XK_1,                      0) /* go to tag 0 */
+	TAGKEYS(                        XK_2,                      1) /* go to tag 1 */
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
@@ -93,7 +93,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }, /* exit DWM */
 };
 
 /* button definitions */
